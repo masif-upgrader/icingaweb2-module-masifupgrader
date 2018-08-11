4f933d4fac85e1fd99d662733735c2ea7ca3dd6f
Alexander A. Klimov <grandmaster@al2klimov.de>
2018-08-11 12:17:11 +0200

    Add optional agent filter to approval form

--- application/forms/Pending/PackagesForm.php
+++ application/forms/Pending/PackagesForm.php
@@ -18,6 +18,16 @@ class PackagesForm extends Form
     protected $tasks;
 
     /**
+     * @var array
+     */
+    protected $agents;
+
+    /**
+     * @var bool
+     */
+    protected $holdOn = false;
+
+    /**
      * @return array
      */
     protected function getTasks()
@@ -46,6 +56,31 @@ EOQ
         return $this->tasks;
     }
 
+    /**
+     * @return array
+     */
+    protected function getAgents()
+    {
+        if ($this->agents === null) {
+            $rawAgents = $this->fetchAll(
+                <<<EOQ
+SELECT a.name, (SELECT COUNT(DISTINCT t1.package) FROM task t1 WHERE t1.agent=a.id AND t1.approved=0)
+FROM agent a
+WHERE EXISTS (SELECT * FROM task t2 WHERE t2.agent=a.id AND t2.approved=0)
+ORDER BY (SELECT COUNT(DISTINCT t1.package) FROM task t1 WHERE t1.agent=a.id AND t1.approved=0) DESC, a.name ASC
+EOQ
+            );
+
+            $this->agents = [];
+
+            foreach ($rawAgents as list($agent, $packages)) {
+                $this->agents[$agent] = $packages;
+            }
+        }
+
+        return $this->agents;
+    }
+
     public function init()
     {
         $this->setName('form_pending_packages');
@@ -64,6 +99,17 @@ EOQ
                     );
                 }
             }
+        }
+
+        if (($this->holdOn = isset($formData['filter_agents_first']) && $formData['filter_agents_first'])
+            || (isset($formData['filter_agents']) && $formData['filter_agents'])) {
+            $this->addElement('hidden', 'filter_agents', ['value' => '1']);
+
+            foreach ($this->getAgents() as $agent => $packages) {
+                $this->addHeadlessElement('checkbox', 'agent_' . bin2hex($agent), []);
+            }
+        } else {
+            $this->addHeadlessElement('checkbox', 'filter_agents_first', []);
         }
     }
 
@@ -134,11 +180,32 @@ EOQ
             $result .= '<tr>' . implode('', $row) . '</tr>';
         }
 
-        return "$result<tr><td colspan='4'></td><td colspan='2'>{$this->getElement('btn_submit')->render($view)}</td></tr></tbody></table>";
+        if ($this->getElement('filter_agents') === null) {
+            $filterAgents = $this->getElement('filter_agents_first');
+
+            $result .= "<tr><td rowspan='2' colspan='4'></td><td colspan='2'>{$filterAgents->render($view)}"
+                . "<label for='{$view->escape($filterAgents->getId())}'>&emsp;{$view->escape($this->translate('Select specific agents first'))}</label></td></tr>"
+                . "<tr><td colspan='2'>{$this->getElement('btn_submit')->render($view)}</td></tr></tbody></table>";
+        } else {
+            $result .= "</tbody></table><table class='common-table'><thead><tr><th colspan='2'>{$view->escape($this->translate('Agent, pending packages'))}</th></tr></thead><tbody>";
+
+            foreach ($this->getAgents() as $agent => $packages) {
+                $approve = $this->getElement('agent_' . bin2hex($agent));
+                $result .= "<tr><td>{$approve->render($view)}<label for='{$view->escape($approve->getId())}'>&emsp;{$view->escape($agent)}</label></td><td>$packages</td></tr>";
+            }
+
+            $result .= "<tr><td colspan='2'>{$this->getElement('btn_submit')->render($view)}</td></tr></tbody></table>";
+        }
+
+        return $result;
     }
 
     public function onSuccess()
     {
+        if ($this->holdOn) {
+            return false;
+        }
+
         // TODO
         return true;
     }
